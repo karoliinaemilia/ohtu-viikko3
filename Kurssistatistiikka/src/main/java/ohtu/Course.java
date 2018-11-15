14b74b072004af567003fd1dcfdbbffa05b6244e
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-11-15 23:55:46 +0200

    tehtava2

---
+++ Kurssistatistiikka/src/main/java/ohtu/Course.java
@@ -0,0 +1,63 @@
+package ohtu;
+
+import java.util.stream.IntStream;
+
+public class Course {
+    
+    private String name;
+    private String fullName;
+    private String term;
+    private String year;
+    private int[] exercises;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getYear() {
+        return year;
+    }
+    
+    
+    public int getWeekAmount(int week) {
+        return exercises[week];
+    }
+
+    public int getAmount() {
+        return IntStream.of(exercises).sum();
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+    
+}
--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -16,7 +16,7 @@ public class Main {
         }
 
         String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
-
+        
         String bodyText = Request.Get(url).execute().returnContent().asString();
 
         System.out.println("opiskelijanumero " + studentNr);
@@ -25,17 +25,30 @@ public class Main {
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
         
+        url = "https://studies.cs.helsinki.fi/courses/courseinfo";
+        bodyText = Request.Get(url).execute().returnContent().asString();
+        
+        Course[] courses = mapper.fromJson(bodyText, Course[].class);
+        
         int teht = 0;
         int aika = 0;
         
-        for (Submission submission : subs) {
-            System.out.println("  " + submission.toString());
-            aika += submission.getHours();
-            teht += submission.getDone();
+        for (Course course: courses) {
+            System.out.println(course.getFullName() + " " + course.getTerm() + " " + course.getYear());
+            System.out.println("");
+            for (Submission sub: subs) {
+                if (sub.getCourse().equals(course.getName())) {
+                    System.out.println("viikko " + sub.getWeek() + ":");
+                    System.out.println("  tehtyjä tehtäviä " + sub.getDone() + "/" + course.getWeekAmount(sub.getWeek()) + " aikaa kului " + sub.getHours() + " tehdyt tehtävät: " + sub.tehtavat() );
+                    aika += sub.getHours();
+                    teht += sub.getDone();
+                }
+            }
+            System.out.println("");
+            System.out.println("yhteensä: " + teht + "/" + course.getAmount() + " tehtävää " + aika + " tuntia");
+            System.out.println("");
+            teht = 0;
+            aika = 0;
         }
-        
-        System.out.println("");
-        System.out.println("yhteensä: " + teht + " tehtävää " + aika + " tuntia");
-
     }
 }
